6a0017a5a6ece17a74f98b95f1134946a211238c
Thabb <49516328+Thabb@users.noreply.github.com>
2023-11-29 16:40:09 +0100

    UPDATE: Variable names and add datetime at the start and end of simulation

--- flower/flower_main.py
+++ flower/flower_main.py
@@ -1,3 +1,4 @@
+import datetime
 from typing import List, Tuple
 
 import torch
@@ -26,12 +27,10 @@ CLASSES = (
     "ship",
     "truck",
 )
-NUM_CLIENTS = 10
-BATCH_SIZE = 32
+NUM_CLIENTS = 5000
+BATCH_SIZE = 1
 
 train_loaders, val_loaders, test_loader = load_datasets(NUM_CLIENTS, BATCH_SIZE)
-train_loader = train_loaders[0]
-val_loader = val_loaders[0]
 net = Net().to(DEVICE)
 
 
@@ -44,11 +43,11 @@ def client_fn(cid: str) -> FlowerClient:
     # Load data (CIFAR-10)
     # Note: each client gets a different trainloader/valloader, so each client
     # will train and evaluate on their own unique data
-    trainloader = train_loaders[int(cid)]
-    valloader = val_loaders[int(cid)]
+    train_loader = train_loaders[int(cid)]
+    val_loader = val_loaders[int(cid)]
 
     # Create a  single Flower client representing a single organization
-    return FlowerClient(net, trainloader, valloader)
+    return FlowerClient(net, train_loader, val_loader)
 
 
 def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
@@ -64,16 +63,18 @@ def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
 strategy = fl.server.strategy.FedAvg(
     fraction_fit=1.0,  # Sample 100% of available clients for training
     fraction_evaluate=0.5,  # Sample 50% of available clients for evaluation
-    min_fit_clients=10,  # Never sample less than 10 clients for training
-    min_evaluate_clients=5,  # Never sample less than 5 clients for evaluation
-    min_available_clients=10,  # Wait until all 10 clients are available
+    min_fit_clients=NUM_CLIENTS,  # Never sample less than 10 clients for training
+    min_evaluate_clients=int(NUM_CLIENTS/2),  # Never sample less than 5 clients for evaluation
+    min_available_clients=NUM_CLIENTS,  # Wait until all 10 clients are available
     evaluate_metrics_aggregation_fn=weighted_average,  # <-- pass the metric aggregation function
 )
 
 # Specify client resources if you need GPU (defaults to 1 CPU and 0 GPU)
 client_resources = None
 if DEVICE.type == "cuda":
-    client_resources = {"num_gpus": 1}
+    client_resources = {"num_gpus": 1, "num_cpus": 1}
+
+print("start time: ", datetime.datetime.now())
 
 # Start simulation
 fl.simulation.start_simulation(
@@ -83,3 +84,5 @@ fl.simulation.start_simulation(
     strategy=strategy,
     client_resources=client_resources,
 )
+
+print("end time: ", datetime.datetime.now())
